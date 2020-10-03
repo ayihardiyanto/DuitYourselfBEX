12094d1251755c09f3f8624c60c1d0f67c6c6130
Hackintosh <hackintosh@Ayis-Mac.local>
2020-10-03 09:35:35 +0700

    login and signup

--- src/app.js
+++ src/app.js
@@ -3,16 +3,37 @@ import express from 'express';
 import parser from 'body-parser';
 import cors from 'cors';
 import routes from './routes/user_routes';
+// import { auth as admin } from 'firebase-admin';
+
+
+// var serviceAccount = require("../config/duityourself-62482-firebase-adminsdk-b64m4-7ac3a8a6f5.json");
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://duityourself-62482.firebaseio.com"
+// });
+
+// function checkAuth(req, res, next) {
+//     if (req.headers.authtoken) {
+//         admin.auth().verifyIdToken(req.headers.authtoken)
+//             .then(() => {
+//                 next()
+//             }).catch(() => {
+//                 res.status(403).send('Unauthorized')
+//             });
+//     } else {
+//         res.status(403).send('Unauthorized')
+//     }
+// }
 
 let app = express()
 app.use(cors())
-app.use(parser.json())
+app.use(parser.urlencoded({
+    extended : false
+}))
 app.use(routes)
 
-// app.get('/', (req, res)=>{
-//     res.send('COMIN')
-// })
 const port = 3210;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,7 +1,31 @@
 import { Router } from 'express';
 import firebase from '../config/db_config';
+import admin from 'firebase-admin';
+
+
+var serviceAccount = require("../config/duityourself-62482-firebase-adminsdk-b64m4-7ac3a8a6f5.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://duityourself-62482.firebaseio.com"
+});
+
+// function checkAuth(req, res, next) {
+//     if (req.headers.authtoken) {
+//         admin.auth().verifyIdToken(req.headers.authtoken)
+//             .then(() => {
+//                 next()
+//             }).catch(() => {
+//                 res.status(403).send('Unauthorized')
+//             });
+//     } else {
+//         res.status(403).send('Unauthorized')
+//     }
+// }
 
 let db = firebase.firestore();
+
+
 
 export default Router().get('/get-user', async (req, res) => {
     let output = [];
@@ -20,6 +44,24 @@ export default Router().get('/get-user', async (req, res) => {
         })
 
     res.send(output);
-}).post('/post-user', async (req, res)={
-    
+}).post('/create-user', async (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    await db.collection('user')
+        .doc(req.body.email).set(user).then(function (docRef) {
+            console.log("Document written with ID: ", docRef);
+            admin.auth().createUser(user).then(function (userRecord) {
+                console.log('this is the user', userRecord);
+            })
+        }).catch((_) => console.log(`this is ${_}`))
+
+
+
+
+    res.send({
+        'statusCode': '201', 
+        'message': 'User Has Been Created'
+    })
 })
